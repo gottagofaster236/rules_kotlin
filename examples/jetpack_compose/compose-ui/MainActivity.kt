b7efae8b2081bfa3aeba39d749780a54ddf6c09f
Lev Leontev <lev.leontev@jetbrains.com>
2024-04-15 07:13:40 +0200

    icon compose preview example

--- examples/jetpack_compose/compose-ui/MainActivity.kt
+++ examples/jetpack_compose/compose-ui/MainActivity.kt
@@ -1,11 +1,17 @@
 package cm.ben.android.bazel.compose.example.ui
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,4 +24,24 @@ class MainActivity : AppCompatActivity() {
 
 @Preview
 @Composable
-fun Greeting(name: String) = Text(text = "Hello $name!")
+fun Greeting(name: String = "world") {
+  var bitmapState by remember{ mutableStateOf<Bitmap?>(null) }
+  val context = LocalContext.current
+
+  LaunchedEffect(Unit) {
+    bitmapState = BitmapFactory.decodeStream(context.assets.open("icon.png"))
+  }
+
+
+  if (null != bitmapState) {
+    val bitmap = bitmapState!!.asImageBitmap()
+    Image(
+      bitmap = bitmap,
+      "assetsImage",
+      colorFilter = null
+    )
+  }
+
+
+  Text(text = "Hello\n$name!", fontSize = 100.sp)
+}
